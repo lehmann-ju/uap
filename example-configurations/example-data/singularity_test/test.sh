0c211876bcaff89891363e4dbc001868bff52074
Julienne Lehmann <72375438+lehmann-ju@users.noreply.github.com>
2020-12-16 06:16:18 +0100

    Update test.sh

--- example-configurations/example-data/singularity_test/test.sh
+++ example-configurations/example-data/singularity_test/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Testing uap"
-    /bin/bash -c "source /.start_uap_python3_v1.0; uap --version; uap /opt/uap/example-configurations/test.yaml run-locally --force"
+    /bin/bash -c "source /.start_uap_python3_v1.0; uap --version; uap /opt/uap/example-configurations/test.yaml run-locally"
